2b6f9bcb4e8f1e5aa0722062414382a4dc2a8cdc
Steve Krenn <stevekrenn@gmail.com>
2023-02-06 19:29:15 -0600

    added mongoose and connection.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log('Hello from server!')
+const path = require('path')
+const express = require('express')
+const connection = require('./config/connection')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+})
+
+//mongoose function, runs only once when DB is opened
+connection.once('open', async () => {
+    app.listen(PORT, () => {
+        console.log(`Express server listening on http://localhost:${PORT}`)
+    })
+})
