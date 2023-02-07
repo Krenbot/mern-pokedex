4793d630c69179252a6fa2556d0366e4c2ac5f56
Steve Krenn <stevekrenn@gmail.com>
2023-02-06 20:33:51 -0600

    seed script successful

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -11,12 +11,39 @@ connection.once('open', async () => {
     //seed pokemon
     for (const id of [1, 2, 3]) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        console.log(response)
-    }
+        const { id: pokemonId,
+            name,
+            moves,
+            height,
+            weight,
+            base_experience,
+            sprites
+        } = await response.json()
 
+        await Pokemon.create({
+            pokemonId,
+            name,
+            height,
+            weight,
+            base_experience,
+            //brakets are necessary to escape the - in the API call
+            image: sprites.other['official-artwork'].front_default,
+            //returns array of strings from inital array
+            moves: moves.map(moveData => moveData.move.name)
+        })
+    }
     //seed trainer
+    const allPokemon = await Pokemon.find()
+    const allPokemonIds = allPokemon.map(pokemon => pokemon._id)
+
+    //create Trainer
+    await Trainer.create({
+        username: 'Ash Ketchum',
+        email: "ash@gmail.com",
+        password: 'masterball',
+        pokemon: allPokemonIds
+    })
 
     console.log('Gotta catch \'em all! - TRAINERS & POKEMON seeded!')
     process.exit(0)
-
 })
