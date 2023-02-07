b16e009750a71c380964606419e9c6164ae6cbbc
Steve Krenn <stevekrenn@gmail.com>
2023-02-06 21:03:52 -0600

    prototype resolvers added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const resolvers = {
+    Query: {
+        pokemons: async (parent, args, context, info) => {
+
+        },
+        pokemons: async (parent, args, context, info) => {
+
+        },
+        trainers: async (parent, args, context, info) => {
+
+        },
+        trainer: async (parent, args, context, info) => {
+
+        },
+    }
+
+}
+
+module.exports = resolvers
